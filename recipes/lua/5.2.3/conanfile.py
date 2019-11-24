8c2ae68aec54f59e78b7ce8e4c5d81a364e91449
Tim Simpson <timsimpson4@gmail.com>
2019-11-24 16:15:57 -0600

    adds a semi-working Lua cmake file

---
+++ recipes/lua/5.2.3/conanfile.py
@@ -0,0 +1,44 @@
+import conans
+import os
+import os.path
+import pathlib
+import shutil
+
+
+GEN_SRC = pathlib.Path('gen_src')
+
+
+class LuaConan(conans.ConanFile):
+    name = "Lua"
+    version = "5.2.3"
+    # settings = "os", "compiler", "build_type", "arch"
+    # exports = ["conanfile.py", "CMakeLists.txt"]
+    # lua_dir = pathlib.Path(f'lua-{version}')
+
+    # Where the extract file contents end up.
+    lua_dir = pathlib.Path(f'lua-{version}')
+
+
+    def source(self):
+        # Download the official Lua sources
+        # conans.tools.get(**self.conan_data["sources"][self.version])
+        shutil.copy(pathlib.Path(__file__).parent / "CMakeLists.txt",
+                    self.lua_dir / "CMakeLists.txt")
+
+    def build(self):
+        cmake = conans.CMake(self)
+        lua_dir = pathlib.Path(self.source_folder) / self.lua_dir
+        cmake.configure(source_folder=str(lua_dir))
+        cmake.build()
+
+    def package(self):
+        cmake = conans.CMake(self)
+        cmake.configure()
+        cmake.install()
+
+        # self.copy(pattern="*.h", dst="include", src=os.path.join(self.lua_dir, "src"))
+        # self.copy(pattern="*.lib", dst="lib", src="lib")
+        # self.copy(pattern="*.a", dst="lib", src="lib")
+
+    def package_info(self):
+        self.cpp_info.libs = ["lua"]
