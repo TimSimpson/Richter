7d11f97ded8ff812f52b12b77643a3c16bc3d4ba
Tim Simpson <timsimpson4@gmail.com>
2019-11-30 17:42:13 -0600

    made bgfx download stuff

--- recipes/bgfx/conanfile.py
+++ recipes/bgfx/conanfile.py
@@ -10,25 +10,41 @@ GEN_SRC = pathlib.Path('gen_src')
 
 class BgfxConan(conans.ConanFile):
     name = "bgfx"
-    version = "2019-11-25"
-    # settings = "os", "compiler", "build_type", "arch"
-    # exports = ["conanfile.py", "CMakeLists.txt"]
-    # lua_dir = pathlib.Path(f'lua-{version}')
+    version = "2019-11-27"
+    settings = "os", "compiler", "build_type", "arch"
 
-    # Where the extract file contents end up.
-    lua_dir = pathlib.Path(f'lua-{version}')
+    repos = ["bgfx", "bimg", "bx"]
 
-    projects = ["bgfx", "bimg", "bx"]
 
+    @property
+    def source_path(self):
+        return pathlib.Path(self.source_folder)
+
+    @property
+    def build_path(self):
+        return pathlib.Path(self.build_folder)
 
     def source(self):
         # Download the official Lua sources
-        for project in self.projects:
-            # conans.tools.get(
-            #     **self.conan_data["sources"][f"{project}/{self.version}"]
-            # )
-            os.rename(f"{project}-master", project)
+        for repo in self.repos:
+            conans.tools.get(
+                **self.conan_data["sources"][f"{repo}/{self.version}"]
+            )
+            # The unzipped directory takes the form of "{repo}-<some-guid>"
+            unzipped_directories = [
+                d for d in self.source_path.iterdir()
+                if d.name.startswith(f"{repo}-")
+            ]
+            if len(unzipped_directories) != 1:
+                raise RuntimeError("Expected only one unzipped directory.")
 
+            os.rename(unzipped_directories[0], repo)
+
+    def build(self):
+        for repo in self.repos:
+            shutil.copytree(self.source_path / repo, self.build_path / repo)
+
+        self.run("make", run_environment=True)
 
 
         # shutil.copy(pathlib.Path(__file__).parent / "CMakeLists.txt",
